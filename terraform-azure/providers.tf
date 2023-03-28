51dc22f25a7d552bd49d12dce3742a374d75e39b
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-28 16:25:03 +0300

    test

---
+++ terraform-azure/providers.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+  
+}
+terraform {
+  
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.47.0"
+      
+
+    }
+    
+  }
+    backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstateitsprout"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    }
+}
+
+
+
+
---
+++ terraform-azure/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "aks_tf_test"
+location = "EastUS2"
+cluster_name = "devops-test-aks"
+kubernetes_version = "1.24.9"
+system_node_count = 1
+
